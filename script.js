da2e23dc2fb56172ecf73d36b1643992ddbee299
Kostaintyn <kostiantyn208148@student-schoolsofwestfield.org>
2024-10-11 16:12:15 +0000

    123

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 /* Nim Trainer by Kostiantyn Lohin */
+/* chart: https://lucid.app/lucidchart/169f84c6-72b2-4492-85df-b103daf884b8/edit?page=0_0&invitationId=inv_140e244d-5aaa-4a42-877d-c567026622b6# */
 
-// global variable
+
+/* global variable */
 var trainer = false; 
 var count = 0; 
 
@@ -18,29 +20,47 @@ function main() {
 
 /* playNim */
 function playNim() {
-    alert("You played Nim");
+    count = 0;
+    while ( count < 20 ) {
+        userTurn();
+        if ( count < 20 ) {
+            CPUTurn();
+        }
+        else alert("U lose");
+        userTurn(); 
+        if ( count < 20 ) {
+            CPUTurn();
+        }
+        else if ( count = 20 ) alert("U win");
+    }
 }
 
-
-/** 
- * main lets user control game type and playing again * 
- * @param parameter1 Description of the first parameter. 
- * @param parameter2 Description of the second parameter. 
- * @return Description of the return value. 
- * 
- *//** 
+ /** 
  * playNim runs Nim, player first and then computer, and says who wins * 
  * @param parameter1 Description of the first parameter. 
  * @param parameter2 Description of the second parameter. 
  * @return Description of the return value. 
- * 
- *//** 
+ */
+
+function userTurn() {
+   /* count += 3; */
+    count = prompt("say namber")
+    alert("You counted 3. Count is now " + count);
+} 
+
+ /** 
  * userTurn gets a turn the user and checks if it is cheating with recursion * 
  * @param parameter1 Description of the first parameter. 
  * @param parameter2 Description of the second parameter. 
  * @return Description of the return value. 
- * 
- *//** 
+ */
+
+function CPUTurn() {
+    count += 1;
+    alert("I counted 3. Count is now " + count);
+}
+
+/** 
  * CPUTurn gets a computer's turn without losing on purpose. Different * 
  * @param parameter1 Description of the first parameter. 
  * @param parameter2 Description of the second parameter. 
